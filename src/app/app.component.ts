cba60d670cf7a64678b8d87765c572c2574e3a6c
Rodion Sibov <rodionsibov@gmail.com>
2022-03-19 22:55:24 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   newMemberName = ''
-  members: string[] = []
+  members: string[] = ['sfsdfs', 'sdfsdfds']
   errorMessage = ''
   numberOfTeams: number | '' = ''
   teams: string[][] = []
