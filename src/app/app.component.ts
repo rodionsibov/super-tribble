686a9275d3e62b99772243f4b42696f967e6de9c
Rodion Sibov <rodionsibov@gmail.com>
2022-03-19 21:05:11 +0200

    udpate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,18 +27,20 @@ export class AppComponent {
 
     const allMembers = [...this.members]
 
-    for (let i = 0; i < this.numberOfTeams; i++) {
-      const randomIndex = Math.floor(Math.random() * allMembers.length)
-      const member = allMembers.splice(randomIndex, 1)[0]
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length)
+        const member = allMembers.splice(randomIndex, 1)[0]
 
-      if (this.teams[i]) {
-        this.teams[i].push(member)
-      } else {
-        this.teams[i] = [member]
+        if (this.teams[i]) {
+          this.teams[i].push(member)
+        } else {
+          this.teams[i] = [member]
+        }
       }
     }
     console.log(this.teams);
-    
+
   }
 
   addMember() {
